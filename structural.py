9067fc115e339398501d12911f74fbfa2ad6f159
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 01:26:11 +0300

    define class

---
+++ structural.py
@@ -0,0 +1,6 @@
+#proxy
+#define class which represents the real object responsible for streaming videos
+class VideoStreamer:
+    def stream_video(self, video_name):
+        # simulate streaming a video
+        return f"Streaming '{video_name}' in high quality!"
