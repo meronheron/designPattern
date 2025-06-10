08a62c32b768d33728e501848459b5dfa5b6ce37
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 00:44:26 +0300

    create __new__

--- singlton.py
+++ singlton.py
@@ -3,3 +3,11 @@
 class President:
     # Class variable to store the single instance
     _instance = None
+    #controls instance creation, ensuring only one instance is created
+    def __new__(cls, name):
+     # Check if a president instance already exists
+        if cls._instance is None:
+            # Create a new instance if none exists
+            cls._instance = super(President, cls).__new__(cls)
+        # Return the single instance
+        return cls._instance
