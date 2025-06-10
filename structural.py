fddd2bbe8b856485283ed2204ae8c35aa3d5f5db
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 01:32:38 +0300

    added VideoStreamerProxy class with is_premium_user parameter and video_streamer instance

--- structural.py
+++ structural.py
@@ -6,3 +6,7 @@ class VideoStreamer:
         return f"Streaming '{video_name}' in high quality!"
 # Proxy that controls access to the video streamer
 class VideoStreamerProxy:
+    def __init__(self, is_premium_user): # whether the user is a premium subscrption
+        self.is_premium_user = is_premium_user
+        # Create the real video streamer
+        self.video_streamer = VideoStreamer()
