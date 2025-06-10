2b61177d3aaf2da97902b91a0f062219a0507b7d
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 00:34:42 +0300

    class  created

---
+++ singlton.py
@@ -0,0 +1,5 @@
+# creational design pattern
+## create class that implements the Singleton pattern to ensure only one instance exists.
+class President:
+    # Class variable to store the single instance
+    _instance = None
