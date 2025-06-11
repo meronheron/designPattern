ca30fd948c8acebf6f0c6889378fa0134f250214
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 15:07:48 +0300

    created the real subject lass

--- proxy.py
+++ proxy.py
@@ -4,3 +4,7 @@ class PaymentService(ABC):#subject class to provide common interface
     @abstractmethod
     def process_payment(self, amount: float) -> str:
         pass
+class RealPaymentService(PaymentService):# real subject class that implements the PaymentService interface
+    def process_payment(self, amount: float) -> str:
+        # Simulate processing a payment
+        return f"Processed payment of ${amount:.2f} successfully."
