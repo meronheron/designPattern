4f77ab8c4202f4e128b7bcba8b3d825d0cfe45b6
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 15:06:03 +0300

    created a subject class

--- proxy.py
+++ proxy.py
@@ -1,1 +1,6 @@
 #structural design pattern
+from abc import ABC, abstractmethod
+class PaymentService(ABC):#subject class to provide common interface
+    @abstractmethod
+    def process_payment(self, amount: float) -> str:
+        pass
