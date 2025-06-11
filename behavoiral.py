881c162e568fa52f3d2dbf5c98538f9d481f057d
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 11:34:47 +0300

    call the strategies from context

--- behavoiral.py
+++ behavoiral.py
@@ -6,16 +6,17 @@ class PaymentStrategy:# Strategy Interface: Base for all payment methods
 # Concrete Strategy 1: Paying with Cash
 class CashPayment(PaymentStrategy):
     def pay(self, amount):
-        return f"Paid ${amount} with cash. Handed over bills!"
+        return f"Paid {amount}ETB with cash. Handed over bills!"
 # Concrete Strategy 2: Paying with ATM Card
-class CreditCardPayment(PaymentStrategy):
+class AtmCardPayment(PaymentStrategy):
     def pay(self, amount):
-        return f"Paid ${amount} with ATM card. Transaction approved!"
+        return f"Paid {amount} ETB with ATM card. Transaction approved!"
 # Concrete Strategy 3: Paying with Mobile App
 class MobileAppPayment(PaymentStrategy):
     def pay(self, amount):
-        return f"Paid ${amount} with mobile app. Scanned QR code!"
-class PizzaShop:# context: The Pizza Shop that processes payments
+        return f"Paid ${amount}ETB with mobile app. Scanned QR code!"
+    
+class PizzaShop:# context: The Pizza Shop that processes payments 
     def __init__(self, payment_strategy):
         self.payment_strategy = payment_strategy  # Holds the chosen strategy
 
@@ -26,3 +27,11 @@ class PizzaShop:# context: The Pizza Shop that processes payments
         # PizzaShop calls the strategy's pay method
         result = self.payment_strategy.pay(amount)
         return result
+shop = PizzaShop(CashPayment())  #cash payment
+print(shop.checkout(560))  # PizzaShop calls CashPayment's pay method
+
+shop.set_payment_strategy(AtmCardPayment())#ATM Card payment
+print(shop.checkout(560))  # PizzaShop calls AtmCardPayment's pay method
+
+shop.set_payment_strategy(MobileAppPayment())#Mobile App payment
+print(shop.checkout(560))# PizzaShop calls MobileAppPayment's pay method
