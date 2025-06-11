5dd5e861c169d68e7bb56798c61d3d7b05890eb7
meronheron <merongetaneh.sel9@gmail.com>
2025-06-11 11:18:31 +0300

    changed to strategy method defined strategy interface and 1 concrete strategy for payment with cash

--- behavoiral.py
+++ behavoiral.py
@@ -1,35 +1,9 @@
-#command
-from abc import ABC, abstractmethod
-class TV:#reciever 
-    def __init__(self):
-        self.is_on = False
-        self.channel = 1
-    def turn_on(self):
-        self.is_on = True
-        print("TV is now turned on.")
-    def turn_off(self):
-        self.is_on = False
-        print("TV is now turned off.")
-    def change_channel(self, channel):
-        if self.is_on:
-            self.channel = channel
-            print(f"Channel changed to {channel}.")
-        else:
-            print("TV is off. Please turn it on first.")
-    def get_state(self):#return the state of the tv
-        return self.is_on, self.channel
-class Command(ABC):#command interface
-    @abstractmethod
-    def execute(self):
-        pass
+#strategy
+class PaymentStrategy:# Strategy Interface: Base for all payment methods
+    def pay(self, amount):
+        pass  # every strategy must have a 'pay' method
 
-    @abstractmethod
-    def undo(self):
-        pass
-class TurnOnCommand(Command):#concrete command
-    def __init__(self, tv: TV):
-        self.tv = tv
-        self.previous_state = tv.get_state()
-    def execute(self):#saves the current state (for undo)
-        self.previous_state = self.tv.get_state()
-        self.tv.turn_on()
+# Concrete Strategy 1: Paying with Cash
+class CashPayment(PaymentStrategy):
+    def pay(self, amount):
+        return f"Paid ${amount} with cash. Handed over bills!"
